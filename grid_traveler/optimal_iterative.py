a5cd529271147cfbd1525211522ab403c15ef562
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-03-16 10:09:27 -0400

    Fixed boolean function in optimal recursive, and added optimal iterative solution to grid traveler

--- grid_traveler/optimal_iterative.py
+++ grid_traveler/optimal_iterative.py
@@ -5,30 +5,27 @@ from grid_traveler.test_resources.time_complexity_test_data import time_complexi
 def optimal(n: int, m: int, source: list, target: list) -> int:
     """
     Time Complexity: O(n*m)
-    Space Complexity: O(n + m)
+    Space Complexity: O(n*m)
     """
-    memo: dict = {}
-
-    def grid_traversal(row: int, column: int) -> int:
-        if row == 0 or column == 0:
-            return 0
-        elif row == 1 and column == 1:
-            return 1
-        elif (row, column) in memo:
-            return memo[(row, column)]
-        elif (column, row) in memo:
-            return memo[(column, row)]
-        else:
-            right_path_traversals: int = grid_traversal(row, column - 1)
-            down_path_traversals: int = grid_traversal(row - 1, column)
-            memo[(row, column)] = right_path_traversals + down_path_traversals
-            return memo[(row, column)]
-
+    if grid_is_invalid(n, m):
+        return 0
     if source_and_target_not_at_edges(n, m, source, target):
         n, m = get_sub_grid_from_source_to_target(n, m, source, target)
-        return grid_traversal(n, m)
-    else:
-        return grid_traversal(n, m)
+    dp = [
+        [0 for _ in range(m)] for _ in range(n)
+    ]
+    dp[n - 1][m - 1] = 1
+    for row in range(n - 1, -1, -1):
+        for col in range(m - 1, -1, -1):
+            neighbors: list = get_neighbors(n, m, row, col)
+            if dp[row][col] == 0:
+                for n_row, n_col in neighbors:
+                    dp[row][col] += dp[n_row][n_col]
+    return dp[0][0]
+
+
+def grid_is_invalid(n: int, m: int) -> bool:
+    return n is 0 or m is 0
 
 
 def source_and_target_not_at_edges(n, m, source, target) -> bool:
@@ -46,6 +43,19 @@ def get_sub_grid_from_source_to_target(n, m, source, target) -> tuple:
     return n_from_source_to_target, m_from_source_to_target
 
 
+def get_neighbors(n, m, row, col) -> list:
+    right: tuple = (row, col + 1)
+    down: tuple = (row + 1, col)
+    potential_neighbors: list = [right, down]
+    actual_neighbors: list = []
+    for n_row, n_col in potential_neighbors:
+        if not n_row > (n - 1) and \
+                not n_col > (m - 1):
+            actual_neighbors.append((n_row, n_col))
+    return actual_neighbors
+
+
 if __name__ == '__main__':
     dynamically_generate_tests(time_complexity_test_data, optimal, timed=True)
     run_dynamic_tests()
+
