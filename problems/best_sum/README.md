9af8e7fed9f6335f7a4b09be5ae5d2ab47c28fe5
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-04-05 17:28:55 -0400

    Adde brute force approach to bestSum problem

---
+++ problems/best_sum/README.md
@@ -0,0 +1,5 @@
+# Best Sum
+Write a function that takes in a targetSum an an array of numbers as arguments.
+
+The function should return an array containing the shortest combination of numbers that
+add up to exactly the targetSum. 
---
+++ problems/best_sum/brute_force.py
@@ -0,0 +1,26 @@
+
+
+def best_sum(target_sum: int, numbers: list) -> any:
+    if target_sum == 0:
+        return []
+    elif target_sum < 0:
+        return None
+    else:
+        combinations: list = []
+        for num in numbers:
+            difference: int = target_sum - num
+            result = best_sum(difference, numbers)
+            if result is not None:
+                result = result + [num]
+                combinations.append(result)
+        if len(combinations) != 0:
+            return min(combinations, key=len)
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    print(best_sum(7, [5, 3, 4, 7]))
+    print(best_sum(7, [2, 4]))
+
+
