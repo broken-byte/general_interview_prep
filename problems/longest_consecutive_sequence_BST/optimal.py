918799d868929bfa57eba3c84f07fda97ac02936
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-04-07 17:38:20 -0400

    Added optimal solution to longest consecutive sequence problem

--- problems/longest_consecutive_sequence_BST/optimal.py
+++ problems/longest_consecutive_sequence_BST/optimal.py
@@ -1,9 +1,10 @@
-from collections import deque
-
 from problems.longest_consecutive_sequence_BST.tree_builder import construct_tree_from_nodes, print_level_order
 
 
 class Solution:
+
+    def __init__(self):
+        self.max_length: int = 0
 
     # Definition for a binary tree node.
     class TreeNode:
@@ -13,7 +14,20 @@ class Solution:
             self.right = right
 
     def longestConsecutive(self, root: TreeNode) -> int:
-        pass
+        self.max_length = 0
+        self.dfs(root, None, 0)
+        return self.max_length
+
+    def dfs(self, current_node: TreeNode, parent: any, length: int):
+        if current_node is None:
+            return
+        if parent is not None and parent.val + 1 == current_node.val:
+            length += 1
+        else:
+            length = 1
+        self.max_length = max(self.max_length, length)
+        self.dfs(current_node.left, current_node, length)
+        self.dfs(current_node.right, current_node, length)
 
 
 if __name__ == '__main__':
