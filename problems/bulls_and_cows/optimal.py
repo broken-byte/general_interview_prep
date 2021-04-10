d372ba38f60e2587d3e074ae26334e0224b45595
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-04-10 15:23:28 -0400

    Cleaned up solution code

--- problems/bulls_and_cows/optimal.py
+++ problems/bulls_and_cows/optimal.py
@@ -4,27 +4,33 @@ from collections import Counter
 class Solution:
     def __init__(self):
         self.secret_count = {}
+        self.secret = ""
+        self.guess = ""
+        self.bulls = 0
+        self.cows = 0
 
     def getHint(self, secret: str, guess: str) -> str:
-        self.create_secret_count(secret)
-        bulls: int = 0
-        cows: int = 0
-        for guess_index, digit in enumerate(guess):
-            if digit in self.secret_count:
-                if secret[guess_index] == digit:
-                    bulls += 1
-                    cows -= 1 if self.secret_count[digit] <= 0 else 0
-                else:
-                    cows += 1 if self.secret_count[digit] > 0 else 0
-                self.secret_count[digit] -= 1
+        self.create_secret_count()
+        self.secret = secret
+        self.guess = guess
+        self.look_for_bovines()
+        return self.create_answer()
 
-        return self.create_answer(bulls, cows)
-
-    def create_secret_count(self, secret: str) -> Counter:
+    def create_secret_count(self):
         self.secret_count = Counter(secret)
 
-    def create_answer(self, bulls: int, cows: int) -> str:
-        return f"{bulls}A{cows}B"
+    def look_for_bovines(self):
+        for guess_index, digit in enumerate(self.guess):
+            if digit in self.secret_count:
+                if self.secret[guess_index] == digit:
+                    self.bulls += 1
+                    self.cows -= 1 if self.secret_count[digit] <= 0 else 0
+                else:
+                    self.cows += 1 if self.secret_count[digit] > 0 else 0
+                self.secret_count[digit] -= 1
+
+    def create_answer(self) -> str:
+        return f"{self.bulls}A{self.cows}B"
 
 
 if __name__ == '__main__':
