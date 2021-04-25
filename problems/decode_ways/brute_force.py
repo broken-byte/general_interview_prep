bb6b1ad31084e53644abb7a244cbac1ced7db0d5
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-04-25 19:43:28 -0400

    Added decode ways brute force solution, working on optimal one

--- problems/decode_ways/brute_force.py
+++ problems/decode_ways/brute_force.py
@@ -1,46 +1,59 @@
 
 
-def brute_force(s: str) -> int:
-    """
-    This method COULD work if you
-    somehow keep track of the order
-    of the sub-arrays as they appear
-    in the original string, but I'm
-    gonna move on to the optimal
-    DP solution to this problem
-    since that MIGHT be the only
-    way to solve this and I need
-    to practice DP anyway LOL.
-    """
-    alphabet_map: dict = generate_alphabet_map()
-    print(alphabet_map)
-    contiguous_sub_arrays: list = get_all_contiguous_sub_arrays_of_string(s)  # O(n^3)
-    print(contiguous_sub_arrays)
-    for i, sub_array in enumerate(contiguous_sub_arrays):
-        if int(sub_array) not in alphabet_map:
-            contiguous_sub_arrays.pop(i)
-    print(contiguous_sub_arrays)
-    return 0
+class Decoder:
+    def __init__(self):
+        self.alphabet_map = {}
+        self.encoded_message: str = ""
 
+    def get_number_of_possible_ways_to_decode(self, s: str) -> int:
+        self.encoded_message = s
+        self.generate_alphabet_map()
+        return self.decode_recursively(index=0, length=0)
 
-def generate_alphabet_map() -> dict:
-    alphabet_map: dict = {}
-    code: int = 1
-    for letter_value in range(65, 91):
-        alphabet_map[code] = chr(letter_value)
-        code += 1
-    return alphabet_map
+    def generate_alphabet_map(self):
+        self.alphabet_map = {}
+        alphabets = range(65, 91)
+        digits = range(1, 27)
+        for digit, letter in zip(digits, alphabets):
+            self.alphabet_map[digit] = chr(letter)
 
+    def decode_recursively(self, index: int, length: int) -> int:
+        if self.at_end_of_message(index, length):
+            return 1 if self.can_be_decoded(index, length) else 0
+        elif self.at_beginning_of_message(index, length) or self.can_be_decoded(index, length):
+            single_digit_decoding: int = self.decode_recursively(index + length, 1)
+            if self.double_digit_decoding_out_of_bounds(index, length):
+                return single_digit_decoding
+            else:
+                double_digit_decoding: int = self.decode_recursively(index + length, 2)
+                return single_digit_decoding + double_digit_decoding
+        else:
+            return 0
 
-def get_all_contiguous_sub_arrays_of_string(s: str) -> list:
-    n: int = len(s)
-    contiguous_sub_arrays: list = []
-    for i in range(n):
-        for j in range(i, n):
-            sub_array: str = s[i: j + 1]
-            contiguous_sub_arrays.append(sub_array)
-    return contiguous_sub_arrays
+    def at_end_of_message(self, index: int, length: int) -> bool:
+        return index + length == len(self.encoded_message)
+
+    def at_beginning_of_message(self, index: int, length: int) -> bool:
+        return index == 0 and length == 0
+
+    def double_digit_decoding_out_of_bounds(self, index: int, length: int) -> bool:
+        return index + length + 2 > len(self.encoded_message)
+
+    def can_be_decoded(self, index: int, length: int) -> int:
+        sub_string = self.encoded_message[index:index + length]
+        if sub_string[0] == "0":
+            return False
+        digit = int(sub_string)
+        can_be_decoded: bool = digit in self.alphabet_map
+        return can_be_decoded
 
 
 if __name__ == '__main__':
-    brute_force("226")
+    decoder = Decoder()
+    print(decoder.get_number_of_possible_ways_to_decode("226"))
+    print(decoder.get_number_of_possible_ways_to_decode("2125"))
+    print(decoder.get_number_of_possible_ways_to_decode("01"))
+    print(decoder.get_number_of_possible_ways_to_decode("12"))
+    print(decoder.get_number_of_possible_ways_to_decode("0"))
+    print(decoder.get_number_of_possible_ways_to_decode("06"))
+    print(decoder.get_number_of_possible_ways_to_decode("111111111111111111111111111111111111111111111"))
--- problems/decode_ways/optimal.py
+++ problems/decode_ways/optimal.py
@@ -1,4 +1,61 @@
 
 
-def num_decodings(self, s: str) -> int:
-    pass
+class Decoder:
+    # TODO: Refactor this so that the recursion parameter
+    #  is the substring being decoded, (which is hashable)
+    def __init__(self):
+        self.alphabet_map: dict = {}
+        self.encoded_message: str = ""
+        self.memo: dict = {}
+
+    def get_number_of_possible_ways_to_decode(self, s: str) -> int:
+        self.encoded_message = s
+        self.generate_alphabet_map()
+        return self.decode_recursively(index=0, decoding_step=0)
+
+    def generate_alphabet_map(self):
+        self.alphabet_map = {}
+        alphabets = range(65, 91)
+        digits = range(1, 27)
+        for digit, letter in zip(digits, alphabets):
+            self.alphabet_map[digit] = chr(letter)
+
+    def decode_recursively(self, index: int, decoding_step: int) -> int:
+        if self.at_end_of_message(index, decoding_step):
+            return 1 if self.can_be_decoded(index, decoding_step) else 0
+        elif self.at_beginning_of_message(index, decoding_step) or self.can_be_decoded(index, decoding_step):
+            single_digit_decoding: int = self.decode_recursively(index + decoding_step, 1)
+            if self.double_digit_decoding_out_of_bounds(index, decoding_step):
+                return single_digit_decoding
+            else:
+                double_digit_decoding: int = self.decode_recursively(index + decoding_step, 2)
+                return single_digit_decoding + double_digit_decoding
+        else:
+            return 0
+
+    def at_end_of_message(self, index: int, decoding_step: int) -> bool:
+        return index + decoding_step == len(self.encoded_message)
+
+    def at_beginning_of_message(self, index: int, decoding_step: int) -> bool:
+        return index == 0 and decoding_step == 0
+
+    def double_digit_decoding_out_of_bounds(self, index: int, decoding_step: int) -> bool:
+        return index + decoding_step + 2 > len(self.encoded_message)
+
+    def can_be_decoded(self, index: int, decoding_step: int) -> int:
+        sub_string = self.encoded_message[index:index + decoding_step]
+        if sub_string[0] == "0":
+            return False
+        digit = int(sub_string)
+        can_be_decoded: bool = digit in self.alphabet_map
+        return can_be_decoded
+
+
+if __name__ == '__main__':
+    decoder = Decoder()
+    print(decoder.get_number_of_possible_ways_to_decode("226"))
+    print(decoder.get_number_of_possible_ways_to_decode("2125"))
+    print(decoder.get_number_of_possible_ways_to_decode("01"))
+    print(decoder.get_number_of_possible_ways_to_decode("12"))
+    print(decoder.get_number_of_possible_ways_to_decode("0"))
+    print(decoder.get_number_of_possible_ways_to_decode("06"))
