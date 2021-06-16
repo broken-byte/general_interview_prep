bbda8269c06934543a945e0dc9b79d20e7d69f86
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-06-16 14:00:36 -0400

    Added some refactoring to past solution

--- problems/merging_two_packages/brute_force.py
+++ problems/merging_two_packages/brute_force.py
@@ -1,15 +1,17 @@
 
 
 def brute_force(arr: list, limit: int) -> list:
-    for i in range(arr):
-        for j in range(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
             if i != j and arr[i] + arr[j] == limit:
                 return get_correctly_ordered_pair(i, j)
     return []
 
 
 def get_correctly_ordered_pair(i, j):
-    if i < j:
-        return [i, j]
-    else:
-        return [j, i]
+    pair: list = [i, j]
+    return sorted(pair, reverse=True)
+
+
+if __name__ == '__main__':
+    print(brute_force([4, 6, 10, 15, 16], 21))
--- problems/merging_two_packages/optimal.py
+++ problems/merging_two_packages/optimal.py
@@ -3,8 +3,8 @@ def optimal(arr: list, limit: int) -> list:
     hashed_values = create_hashed_arr(arr)
     for current_index, key in enumerate(arr):
         difference: int = limit - key
-        diff_index: int = hashed_values[limit - key]
-        if difference in hashed_values and indices_not_equal(diff_index, current_index):
+        diff_index: int = hashed_values[limit - key] if difference in hashed_values else None
+        if diff_index is not None and indices_not_equal(diff_index, current_index):
             ans = correctly_ordered_pair(current_index, diff_index)
             return ans
     return []
@@ -18,11 +18,13 @@ def create_hashed_arr(arr: list):
 
 
 def correctly_ordered_pair(i: int, j: int):
-    if i > j:
-        return [i, j]
-    else:
-        return [j, i]
+    pair: list = [i, j]
+    return sorted(pair, reverse=True)
 
 
 def indices_not_equal(i: int, j: int):
     return i != j
+
+
+if __name__ == '__main__':
+    print(optimal([4, 6, 10, 15, 16], 21))
