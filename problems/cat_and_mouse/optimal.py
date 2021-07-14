09a9f9e74a3ba36dedb0422c7ecefad0f28e9956
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-07-14 14:14:37 -0400

    Refactored a solution to be more readable

--- problems/cat_and_mouse/optimal.py
+++ problems/cat_and_mouse/optimal.py
@@ -11,28 +11,21 @@ class WinnerIs(Enum):
 
 class Solution:
     def catMouseGame(self, graph: list[list[int]]) -> int:
-        GameState = namedtuple('GameState', ['mouse_position', 'cat_position', 'move_count'])
 
         @lru_cache(None)
-        def dp(current_game_state: GameState) -> WinnerIs:
+        def dp(mouse_position: int, cat_position: int, move_count: int) -> WinnerIs:
             # Base Cases
-            if current_game_state.mouse_position == 0:
+            if mouse_position == 0:
                 return WinnerIs.MOUSE
-            elif current_game_state.mouse_position == current_game_state.cat_position:
+            elif mouse_position == cat_position:
                 return WinnerIs.CAT
-            elif current_game_state.move_count > 2 * len(graph):
+            elif move_count > 2 * len(graph):
                 return WinnerIs.DRAW
-
             # Recursive Cases
-            if current_game_state.move_count % 2 == 0:  # Mouse Moves
+            if move_count % 2 == 0:  # Mouse Moves
                 can_draw = False
-                for next_position in graph[current_game_state.mouse_position]:
-                    next_game_state = GameState(
-                        mouse_position=next_position,
-                        cat_position=current_game_state.cat_position,
-                        move_count=current_game_state.move_count + 1
-                    )
-                    result = dp(next_game_state)
+                for next_position in graph[mouse_position]:
+                    result = dp(next_position, cat_position, move_count + 1)
                     if result == WinnerIs.MOUSE:
                         return WinnerIs.MOUSE
                     elif result == WinnerIs.DRAW:
@@ -42,15 +35,10 @@ class Solution:
                 return WinnerIs.CAT
             else:  # Cat Moves
                 can_draw = False
-                for next_position in graph[current_game_state.cat_position]:
+                for next_position in graph[cat_position]:
                     if next_position == 0:
                         continue
-                    next_game_state = GameState(
-                        mouse_position=current_game_state.mouse_position,
-                        cat_position=next_position,
-                        move_count=current_game_state.move_count + 1
-                    )
-                    result = dp(next_game_state)
+                    result = dp(mouse_position, next_position, move_count + 1)
                     if result == WinnerIs.CAT:
                         return WinnerIs.CAT
                     elif result == WinnerIs.DRAW:
@@ -58,5 +46,4 @@ class Solution:
                 if can_draw:
                     return WinnerIs.DRAW
                 return WinnerIs.MOUSE
-        starting_game_state = GameState(1, 2, 0)
-        return dp(starting_game_state).value
+        return dp(1, 2, 0).value
