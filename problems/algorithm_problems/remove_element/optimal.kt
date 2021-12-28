675c78d31d40ae33d66370ac7d9c5fd387853b95
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-12-27 18:24:31 -0600

    Added Kotlin solution for easy problem

---
+++ problems/algorithm_problems/remove_element/optimal.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var left = 0;
+        var right = nums.size - 1
+        while (left <= right) {
+            if (nums[left] == `val`) {
+                nums[left] = nums[right].also { nums[right] = nums[left] }
+                right--
+            }
+            else left++
+        }
+        return left
+    }
+}
